6468cb2b4458d71bca541a9556f76b242f1a09c3
Anton Danylenko <adanylenko@stuy.edu>
2019-06-14 01:32:19 -0400

    s

--- draw.py
+++ draw.py
@@ -25,7 +25,9 @@ def draw_scanline(x0, z0, x1, z1, y, screen, zbuffer, normal0, normal1):
 
     while x <= x1:
         color = get_lighting(normal, view, ambient, light, symbols, reflect )
+        print("###################")
         print("Color: ", color)
+        print("###################")
         plot(screen, zbuffer, color, x, y, z)
         x+= 1
         z+= delta_z
